bfbd2dd2e483233f3e94bf7940d8071e83be6517
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-12-09 13:12:12 -0300

    feat: adds insertion, traversal and other functions regarding BinaryTree

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 struct Node {
     int value;
@@ -86,6 +87,113 @@ void getNodesCount(Node* rootNode, int* count) {
     }
 }
 
+void preOrderTraversal(Node* root) {
+    std::cout << root->value << ' ';
+    
+    if (root->leftChild != nullptr) {
+        preOrderTraversal(root->leftChild);
+    }
+
+    if (root->rightChild != nullptr) {
+        preOrderTraversal(root->rightChild);
+    }
+}
+
+void symmetricalTraversal(Node* root) {
+    if (root->leftChild != nullptr) {
+        symmetricalTraversal(root->leftChild);
+    }
+
+    std::cout << root->value << ' ';
+
+    if (root->rightChild != nullptr) {
+        symmetricalTraversal(root->rightChild);
+    }
+}
+
+void postOrderTraversal(Node* root) {
+    if (root->leftChild != nullptr) {
+        postOrderTraversal(root->leftChild);
+    }
+
+    if (root->rightChild != nullptr) {
+        postOrderTraversal(root->rightChild);
+    }
+
+    std::cout << root->value << ' ';
+}
+
+void levelTraversal(Node* root) {
+    std::queue<Node*> nodes;
+    nodes.push(root);
+
+    while (nodes.size() != 0) {
+        Node* currentNode = nodes.front();
+        std::cout << currentNode->value << ' ';
+
+        if (currentNode->leftChild) {
+            nodes.push(currentNode->leftChild);
+        }
+
+        if (currentNode->rightChild) {
+            nodes.push(currentNode->rightChild);
+        }
+
+        nodes.pop();
+    }
+}
+
+Node* locateMin(Node* root) {
+    std::queue<Node*> nodes;
+    nodes.push(root);
+    Node* min = root;
+
+    while(nodes.size() != 0) {
+        Node* currentNode = nodes.front();
+        if (currentNode->value < min->value) {
+            min = currentNode;
+        }
+
+        if (currentNode->leftChild != nullptr) {
+            nodes.push(currentNode->leftChild);
+        }
+
+        if (currentNode->rightChild != nullptr) {
+            nodes.push(currentNode->rightChild);
+        }
+
+        nodes.pop();
+    }
+    
+    return min;
+}
+
+void removeBST(int value, Node*& node) {
+    Node* auxiliaryPointer = nullptr;
+
+    if (node == nullptr) {
+        return;
+    }
+
+    if (value < node->value) {
+        removeBST(value, node->leftChild);
+    } else if (value > node->value) {
+        removeBST(value, node->rightChild);
+    } else if (node->rightChild != nullptr && node->leftChild != nullptr) {
+        auxiliaryPointer = locateMin(node->rightChild);
+        node->value = auxiliaryPointer->value;
+        removeBST(node->value, node->rightChild);
+    } else {
+        auxiliaryPointer = node;
+
+        if (node->leftChild != nullptr) {
+            node = node->leftChild;
+        } else {
+            node = node->rightChild;
+        }
+    }
+}
+
 std::vector<int> getTreeValues(int valuesCount) {
     std::vector<int> values;
 
@@ -112,15 +220,60 @@ int main() {
     }
 
     std::cout << "Done inserting" << std::endl;
-    displayBST(rootNode);
 
-    int leafCount = 0;
-    getLeavesCount(rootNode, &leafCount);
-    std::cout << "Leaf count: " << leafCount << std::endl;
+    std::cout << "Removing element of key 11, new tree state" << std::endl;
+    removeBST(11, rootNode);
 
-    int nodesCount = 0;
-    getNodesCount(rootNode, &nodesCount);
-    std::cout << "Nodes count: " << nodesCount << std::endl;
+    int option = 1;
 
+    while (option != 0) {
+        std::cout << "What do you wish to do?" << std::endl;
+        std::cout << "[1] Display tree" << std::endl;
+        std::cout << "[2] Nodes count" << std::endl;
+        std::cout << "[3] Leaves count" << std::endl;
+        std::cout << "[4] Pre Order Traversal" << std::endl;
+        std::cout << "[5] Symmetrical Traversal" << std::endl;
+        std::cout << "[6] Post Order Traversal" << std::endl;
+        std::cout << "[7] Level Traversal" << std::endl;
+        std::cout << "[0] Exit" << std::endl;
+        std::cin >> option;
+
+        int nodesCount = 0;
+        int leavesCount = 0;
+
+        switch (option) {
+            case 1:
+                displayBST(rootNode);
+                break;
+            case 2:
+                getNodesCount(rootNode, &nodesCount);
+                std::cout << nodesCount << std::endl;
+                break;
+            case 3:
+                getLeavesCount(rootNode, &leavesCount);
+                std::cout << leavesCount << std::endl;
+                break;
+            case 4:
+                preOrderTraversal(rootNode);
+                std::cout << std::endl;
+                break;
+            case 5:
+                symmetricalTraversal(rootNode);
+                std::cout << std::endl;
+                break;
+            case 6:
+                postOrderTraversal(rootNode);
+                std::cout << std::endl;
+                break;
+            case 7:
+                levelTraversal(rootNode);
+                std::cout << std::endl;
+                break;
+            case 0:
+                break;
+            default:
+                break;
+        }
+    }
     return 1;
 }
